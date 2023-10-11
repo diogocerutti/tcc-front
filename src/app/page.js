01efe2dac27aa4686ef8370f42777420357045b9
diogocerutti <diogo.cerutti19@gmail.com>
2023-10-10 23:27:22 -0300

    First login page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,34 @@
 import styles from "./page.module.css";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export default function Home() {
-  return <main className={styles.main}>Home</main>;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Don'Ana
+          </Typography>
+          <Button color="inherit" href="/login">
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
