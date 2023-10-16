ac62db0f1d05a2cc0f427099c93c7bb115162eed
diogocerutti <diogo.cerutti19@gmail.com>
2023-10-16 12:51:48 -0300

    AppBar Layout

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,13 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Authenticator from "../components/Authenticator";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +20,39 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Don'Ana
+              </Typography>
+              <Button color="inherit" href="/login">
+                Login
+              </Button>
+            </Toolbar>
+            {/*data.map((admin, index) => {
+          return (
+            <div key={index}>
+              <h2>name: {admin.name}</h2>
+              <h2>username: {admin.username}</h2>
+              <h2>password: {admin.password}</h2>
+              <h2>status: {admin.status}</h2>
+
+              <hr />
+            </div>
+          );
+        })*/}
+          </AppBar>
+        </Box>
         <Authenticator />
         {children}
       </body>
